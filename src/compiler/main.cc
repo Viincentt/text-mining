68d96595177717b8350f7641ba170c618a2041cc
Viincentt <vincent.hu@epita.fr>
2020-06-23 00:53:29 +0200

    tmln: compiler: serializing/deserializing done

--- src/compiler/main.cc
+++ src/compiler/main.cc
@@ -1,30 +1,56 @@
 #include <iostream>
 #include <fstream>
 #include "../trie/trie.hh"
+
+void compile(std::ofstream& out, PTrie root)
+{
+    out.write(reinterpret_cast<char *>(&root.f), sizeof(root.f));
+    auto size = root.children.size();
+    out.write(reinterpret_cast<char *>(&size), sizeof(size));
+    for (auto& it: root.children) {
+        auto key = it.first;
+        out.write(reinterpret_cast<char *>(&key), sizeof(key));
+        compile(out, it.second);
+    }
+}
+PTrie get(std::ifstream& input){
+    int freq;
+    input.read(reinterpret_cast<char *>(&freq), sizeof(freq));
+    PTrie res(freq);
+    size_t nb;
+    input.read(reinterpret_cast<char *>(&nb), sizeof(nb));
+    std::map<std::string, PTrie> m;
+    for (size_t i = 0; i < nb; ++i){
+        std::string key;
+        input.read(reinterpret_cast<char *>(&key), sizeof(key));
+        m.insert({key, get(input)});
+    }
+    res.children = m;
+    return res;
+}
 int main(int argc, char const *argv[])
 {
-    /*
-    gestion d'args
-    open txt file
-    creation patricia trie
-    binarisation
-    todo might leak because of ifstream ???
-    */
     if (argc != 3)
         return 1;
-    std::ifstream words(argv[1]);
+    
     std::string word;
     int freq;
-    auto trie = std::make_shared<PTrie>();
-    while (words >> word) {
-        words >> freq;
-        trie->add(word, freq);
-    }
+    auto trie = PTrie();
+    
+    std::ifstream words(argv[1]);
+    while (words >> word && words >> freq) 
+        trie.add(word, freq, 0, word.size());
     words.close();
-    // trie->print();
-    // trie->checkNbWords();
-    // std::cout << "Size of Structure: " << sizeof(*(trie->getRoot())) << "\n";
-    // std::ofstream out(argv[2], std::ios::out | std::ios::binary);
-    // TODO binariser l'arbre ???
+    std::cout << "trie built\n";
+    // trie.print(0);
+    std::ofstream out(argv[2], std::ios::binary);
+    compile(out, trie);
+    out.close();
+    std::cout << "compile finished\n";
+    std::ifstream input(argv[2], std::ios::binary);
+    PTrie t = get(input);
+    input.close();
+    std::cout << "retrieve finished\n";
+    t.print(0);
     return 0;
 }
