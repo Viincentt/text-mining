c853e44af0a3a62206312c19f6324900820f603c
Viincentt <vincent.hu@epita.fr>
2020-06-26 15:08:25 +0200

    tmln: compiler: remove compiler/load ptrie functions

--- src/compiler/main.cc
+++ src/compiler/main.cc
@@ -2,36 +2,12 @@
 #include <fstream>
 #include "../trie/trie.hh"
 
-void compile(std::ofstream& out, PTrie root)
-{
-    out.write(reinterpret_cast<char *>(&root.f), sizeof(root.f));
-    auto size = root.children.size();
-    out.write(reinterpret_cast<char *>(&size), sizeof(size));
-    for (auto& it: root.children) {
-        auto key = it.first;
-        out.write(reinterpret_cast<char *>(&key), sizeof(key));
-        compile(out, it.second);
-    }
-}
-PTrie get(std::ifstream& input){
-    int freq;
-    input.read(reinterpret_cast<char *>(&freq), sizeof(freq));
-    PTrie res(freq);
-    size_t nb;
-    input.read(reinterpret_cast<char *>(&nb), sizeof(nb));
-    std::map<std::string, PTrie> m;
-    for (size_t i = 0; i < nb; ++i){
-        std::string key;
-        input.read(reinterpret_cast<char *>(&key), sizeof(key));
-        m.insert({key, get(input)});
-    }
-    res.children = m;
-    return res;
-}
+
+
 int main(int argc, char const *argv[])
 {
     if (argc != 3)
-        return 1;
+        return 1; // TODO gestion d'erreur
     
     std::string word;
     int freq;
@@ -42,13 +18,12 @@ int main(int argc, char const *argv[])
         trie.add(word, freq, 0, word.size());
     words.close();
     std::cout << "trie built\n";
-    // trie.print(0);
     std::ofstream out(argv[2], std::ios::binary);
     compile(out, trie);
     out.close();
     std::cout << "compile finished\n";
     std::ifstream input(argv[2], std::ios::binary);
-    PTrie t = get(input);
+    PTrie t = load(input);
     input.close();
     std::cout << "retrieve finished\n";
     t.print(0);
