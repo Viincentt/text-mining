d88b7041e7da74aac2b17e50889d157b8c0a6b65
Viincentt <vincent.hu@epita.fr>
2020-07-05 03:31:16 +0200

    tmln: app: ram usage compliant, dist 0/n split

--- src/app/main.cc
+++ src/app/main.cc
@@ -10,7 +10,6 @@
 int DLevenshtein(const std::string& anchor, const std::string& test) {
     auto anchorSize = anchor.size();
     auto testSize = test.size();
-    // TODO boost matrix
     std::vector<std::vector<int>> d(anchorSize + 1, std::vector<int>(testSize + 1));
     for (size_t i = 0; i <= anchorSize; ++i)
         d[i][0] = i;
@@ -32,37 +31,53 @@ int DLevenshtein(const std::string& anchor, const std::string& test) {
 }
 
 // DFS of Patricia trie
-void rec(const PTrie& root, const std::string& s, const int& dist, std::map<int, std::map<int, std::set<std::string>>>& m, const std::string& cur) {
-    if (root.f != -1) {
+void rec(const FTrie& root, const std::string& s, const int& dist, std::map<int, std::map<int, std::set<std::string>>>& m, const std::string& cur) {
+    if (root.freq != -1) {
         auto d = DLevenshtein(s, cur);
         if (d <= dist)
-            m[d][root.f].emplace(cur);
+            m[d][root.freq].emplace(cur);
     }
     for (const auto& elt: root.children)
-        rec(elt.second, s, dist, m, cur + elt.first);
+        rec(elt, s, dist, m, cur + elt.key);
 }
 
-std::map<int, std::map<int, std::set<std::string>>> findWords(const PTrie& root, const std::string& s, const int& dist) {
+std::map<int, std::map<int, std::set<std::string>>> findWords(const FTrie& root, const std::string& s, const int& dist) {
     std::map<int, std::map<int, std::set<std::string>>> res;
-    // std::string buf;
-    rec(root, s, dist, res, ""); 
+    for (const auto &child : root.children)
+        rec(child, s, dist, res, child.key);
     return res;
 }
 
+void recOneWord(const FTrie& root, const std::string& s, size_t i) {
+    if (i >= s.size()) {
+        std::cout << "[{\"word\":\"" << s << "\",\"freq\":" << root.freq << ",\"distance\":0}]";
+        return;
+    }
+    for (const auto& child: root.children) {
+        auto size = child.key.size();
+        if (child.key == s.substr(i, size))
+            recOneWord(child, s, i + size);
+    }
+}
+
 // Print result as json
-void print(std::map<int, std::map<int, std::set<std::string>>> res) {
+void print(std::map<int, std::map<int, std::set<std::string>>> res)
+{
     std::cout << "[";
-    if (res.size() >= 1) {
+    if (res.size() >= 1)
+    {
         auto dist = res.begin();
         auto freq = dist->second.rbegin();
         auto word = freq->second.begin();
-        std::cout << "{\"word\":\"" << *word << "\",\"freq\":" << freq->first << ",\"distance\":" << dist->first << "}"; 
+        std::cout << "{\"word\":\"" << *word << "\",\"freq\":" << freq->first << ",\"distance\":" << dist->first << "}";
         freq->second.erase(word);
     }
-    for (const auto& dist: res) {
-        for (auto freq = dist.second.rbegin(); freq != dist.second.rend(); ++freq) {
-            for (const auto& word: freq->second)
-                std::cout << ",{\"word\":\"" << word << "\",\"freq\":" << freq->first << ",\"distance\":" << dist.first << "}"; 
+    for (const auto &dist : res)
+    {
+        for (auto freq = dist.second.rbegin(); freq != dist.second.rend(); ++freq)
+        {
+            for (const auto &word : freq->second)
+                std::cout << ",{\"word\":\"" << word << "\",\"freq\":" << freq->first << ",\"distance\":" << dist.first << "}";
         }
     }
     std::cout << "]\n";
@@ -70,19 +85,27 @@ void print(std::map<int, std::map<int, std::set<std::string>>> res) {
 
 int main(int argc, char const *argv[])
 {
-    if (argc < 2) 
+    if (argc < 2)
         errx(1, "Usage ./TextMiningApp /path/to/dict.bin");
 
     auto input = std::ifstream(argv[1], std::ios::binary);
-    const PTrie t = load(input);
+    const FTrie t = load(input);
     input.close();
-
     std::string approx;
     int dist;
     std::string word;
+    size_t size;
 
     for (; std::cin >> approx && approx == "approx" && std::cin >> dist && std::cin >> word;) {
-        print(findWords(t, word, dist));
+        if (dist != 0) {
+            print(findWords(t, word, dist)); 
+        } else {
+            for (const auto& child: t.children) {
+                size = child.key.size();
+                if (child.key == word.substr(0, size))
+                    recOneWord(child, word, size);
+            }
+        }
     }
     return 0;
 }
